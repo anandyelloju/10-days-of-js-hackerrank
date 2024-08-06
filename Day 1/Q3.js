b89ada5552c062e1da849bedec6196fba3883e48
Anand Yelloju <65752857+anandyelloju@users.noreply.github.com>
2024-08-06 13:59:24 +0530

    Add Q3.js to Day 1

---
+++ Day 1/Q3.js
@@ -0,0 +1,12 @@
+// Let and Const
+
+function main() {
+    const PI = Math.PI;
+    let r = readLine();
+    
+    // Print the area of the circle:
+    console.log(PI * (r * r));
+    
+    // Print the perimeter of the circle:
+    console.log(2 * PI * r);
+}
