374085613811265fe7c257b7d592a5cb0072da70
Anand Yelloju <65752857+anandyelloju@users.noreply.github.com>
2024-08-08 13:34:43 +0530

    Add Q2.js to Day 2

---
+++ Day 2/Q2.js
@@ -0,0 +1,23 @@
+// Conditional Statements: Switch
+
+function getLetter(s) {
+    let letter;
+    // Write your code here
+    switch(true){
+        case 'aeiou'.includes(s[0]):
+            letter = 'A';
+            break;
+        case 'bcdfg'.includes(s[0]):
+            letter = 'B';
+            break;
+        case 'hjklm'.includes(s[0]):
+            letter = 'C';
+            break;
+        case 'npqrstvwxyz'.includes(s[0]):
+            letter = 'D';
+            break;
+        default:
+            letter = 'Unknown'; 
+    }
+    return letter;
+}
