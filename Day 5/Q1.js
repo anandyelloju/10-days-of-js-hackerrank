19b37765097a6da66d4f1cdbe2bfcf0696bb58b8
Anand Yelloju <65752857+anandyelloju@users.noreply.github.com>
2024-08-16 16:37:52 +0530

    Add Q1.js to Day 5

---
+++ Day 5/Q1.js
@@ -0,0 +1,11 @@
+// Inheritance
+
+Rectangle.prototype.area = function() {
+    return this.w * this.h;
+}
+
+class Square extends Rectangle {
+    constructor(side){
+        super(side, side);
+    }
+}
